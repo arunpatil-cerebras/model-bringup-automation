a2b957e87a98b4458d13b718289667a58639e859
arunpatil-cerebras <arun.patil@cerebras.net>
2025-02-24 08:30:14 -0800

    changes for running on dev server

---
+++ automate_configs_local.py
@@ -0,0 +1,167 @@
+import yaml
+import copy
+
+base_config_file = 'exp_fp16_ND_difft.yaml'
+
+base_run_sh = """
+#!/bin/bash 
+python3 run.py CSX --params {config_file} --model_dir {log_dir} --mode train --compile_only --num_act_servers 1 --mgmt_address localhost:9000
+echo RUN_COMPLETE 
+"""
+
+# Define parameters and options
+param_dict = {}
+param_dict['trainer.init.model.attention_module'] = ['diff_attention', 'aiayn_attention']
+param_dict['trainer.init.model.attention_activation'] = ['softmax', 'sigmoid']
+param_dict['trainer.init.model.num_hidden_layers'] = [12,4]
+param_dict['trainer.init.model.num_heads'] =  [12,4]
+param_dict['trainer.init.model.position_embedding_type'] = ['learned', 'alibi', {'position_embedding_type':'rotary', 'rope_theta':500000, 'rotary_dim':64}]
+param_dict['trainer.fit.train_dataloader.batch_size'] = [256,128,64,8]
+
+# Exclusion list
+exclusions = []
+#exclusions.append({'trainer.init.model.attention_module': 'diff_attention', 'trainer.init.model.position_embedding_type': 'alibi'})
+
+# if any of the higher values susceed, dont iterate throgh lower values
+stop_criteria_params = ['trainer.fit.train_dataloader.batch_size', 'trainer.init.model.num_heads' , 'trainer.init.model.num_hidden_layers']
+
+
+
+def read_yaml_file(filename):
+    with open(filename, 'r') as file:
+        data = yaml.safe_load(file)
+    return data
+
+# Example usage
+
+yaml_data = read_yaml_file(base_config_file)
+print(yaml_data['trainer']['fit']['train_dataloader'])
+
+
+from itertools import product
+import yaml
+import os
+
+
+# Generate all combinations of parameters
+keys, values = zip(*param_dict.items())
+all_combinations = [dict(zip(keys, v)) for v in product(*values)]
+#for combo in all_combinations: 
+#    print(combo)#
+
+# Function to update nested dictionary
+def update_nested_dict(ref_config, combo_key, combo_value):
+
+    #print("looking for combo_key ", combo_key)
+    new_dict = {}
+
+    for key, value in ref_config.items():
+
+        #print("iterating through",  key)
+        if isinstance(value, dict):
+            #print("nested parse",  key)
+            new_dict[key] = update_nested_dict(ref_config[key], combo_key, combo_value)
+            
+        
+        elif(key == combo_key):
+            #print("Match Found ", key, value)
+            if isinstance(combo_value, dict):
+                for k, v in combo_value.items():
+                    new_dict[k] = v
+                    print("inserted : ", k, ":", v)
+            else:
+                new_dict[combo_key] = combo_value
+                #print("inserted : ", combo_key, ":", combo_value)
+        
+        else:
+            new_dict[key] = value
+    
+    return new_dict
+
+
+
+    #return ref_config
+
+
+
+# Filter out combinations that match any exclusion
+valid_combinations = []
+for combination in all_combinations:
+    exclude = False
+    for exclusion in exclusions:
+        if all(combination.get(k) == v for k, v in exclusion.items()):
+            exclude = True
+            break
+    if not exclude:
+        valid_combinations.append(combination)
+
+# Directory to save generated config files
+config_dir = "generated_configs_local"
+os.makedirs(config_dir, exist_ok=True)
+
+# Save each valid combination as a YAML config file
+combo_mapping = {}
+for i, combination in enumerate(valid_combinations):
+    #print("processing  combo : ", combination)
+    new_config = copy.deepcopy(yaml_data)
+    for key, value in combination.items():
+        #print("inserting from Combo ", key, value)
+        key_list = key.split('.')
+        cur_conf  = new_config
+        for key_cur in key_list[:-1]:
+            cur_conf = cur_conf[key_cur]
+        
+        if isinstance(value, dict):
+            for k, v in value.items():
+                cur_conf[k] = v
+        else:
+            cur_conf[key_list[-1]] = value
+
+
+    
+    config_filename = os.path.join(config_dir, f"config_{i+1}.yaml")
+    combo_mapping[f"config_{i+1}.yaml"] = combination
+
+
+    with open(config_filename, 'w') as file:
+        yaml.dump(new_config, file, default_flow_style=False)
+        #print(new_config['trainer']['fit']['train_dataloader'])
+    #break
+
+print(f"Generated {len(valid_combinations)} config files in the '{config_dir}' directory.")
+
+
+
+
+# generate run.sh files
+
+
+# Directory to save run.sh files
+run_sh_dir = "generated_run_scripts_local"
+os.makedirs(run_sh_dir, exist_ok=True)
+
+# Directory for logs/checkpoints
+log_dir_main = "logs"
+os.makedirs(log_dir_main, exist_ok=True)
+
+# Generate run.sh files
+run_mapping = {}
+for k, config_file in enumerate(sorted(os.listdir(config_dir))):
+    config_idx = int(config_file.split('.')[0].split('_')[-1])
+    print(config_idx, config_file)
+    logging_folder = os.path.join(log_dir_main , "log_" + str(config_idx))
+    run_sh_content = base_run_sh.format(config_file=os.path.join(config_dir, config_file), log_dir=logging_folder)
+    run_sh_filename = os.path.join(run_sh_dir, f"run_{config_idx}.sh")
+    with open(run_sh_filename, 'w') as file:
+        file.write(run_sh_content)
+    run_log_file = os.path.join(logging_folder, "latest_run.log")
+    run_mapping[run_sh_filename] = {"Config": config_file, "Run_Log_file":run_log_file, "Combination":combo_mapping[config_file]}
+
+import json
+# Write to JSON file
+json_file = 'run_config_mapping_local.json'
+with open(json_file, 'w') as file:
+    json.dump(run_mapping, file, indent=4)
+print(f"JSON file '{json_file}' has been created.")
+
+
---
+++ automate_runs_devservier.py
@@ -0,0 +1,197 @@
+import json
+
+import multiprocessing
+import subprocess
+import os
+import signal
+import sys
+import re
+import time
+
+### All Settings here
+json_file = "run_config_mapping_local.json"
+parentdir = '/cb/home/arunkp/ws/monolith_r2.4'
+scrpt_str1 = """
+cbrun -- srun -c 32 appliance_emulator -b ws_swmodel -n 1 -a -f /net/ankura-dev/srv/nfs/ankura-data/ws/monolith/src/infra/fabric/sdr_780x1191_cs3.json
+"""
+
+
+def read_mgmt_address(log_file):
+    pattern = r'--mgmt_address (\d+\.\d+\.\d+\.\d+:\d+)'
+    max_attempts = 30
+    attempts = 0
+    while attempts < max_attempts:
+        if os.path.exists(log_file):
+            with open(log_file, 'r') as f:
+                content = f.read()
+                print(content)
+                match = re.search(pattern, content)
+                if match:
+                    return match.group(1)
+        time.sleep(5)
+        print("attempts ", attempts)
+        attempts += 1
+    return None
+
+def update_script2(script2_path, mgmt_address):
+    with open(script2_path, 'r') as f:
+        content = f.read()
+    
+    # Replace or add the management address
+    if '--mgmt_address' in content:
+        updated_content = re.sub(r'--mgmt_address \S+', f'--mgmt_address {mgmt_address}', content)
+    else:
+        updated_content = content.replace('\n', f' --mgmt_address {mgmt_address}\n', 1)
+    
+    with open(script2_path, 'w') as f:
+        f.write(updated_content)
+
+def run_script_cwd(script_name, log_file):
+    if log_file:
+        with open(log_file, 'w') as f:
+            process = subprocess.Popen(['bash', script_name], 
+                                       stdout=f, 
+                                       stderr=subprocess.STDOUT, 
+                                       start_new_session=True)
+    else:
+        process = subprocess.Popen(['bash', script_name], 
+                                   start_new_session=True)
+    return process
+
+def run_script(parent_dir, current_working_directory, script_name, log_file=None):
+
+    os.chdir(parent_dir)
+
+    if log_file:
+        with open(log_file, 'w') as f:
+            process = subprocess.Popen(['bash', script_name], 
+                                       stdout=f, 
+                                       stderr=subprocess.STDOUT, 
+                                       start_new_session=True)
+    else:
+        process = subprocess.Popen(['bash', script_name], 
+                                   start_new_session=True)
+    os.chdir(current_working_directory)
+    return process
+
+def monitor_and_kill(log_compile_file, process_to_kill):
+
+    while(1):
+        with open(log_compile_file, 'r') as f:
+            content = f.read()
+        if('RUN_COMPLETE' in content):
+            os.killpg(os.getpgid(process_to_kill.pid), signal.SIGTERM)
+            break
+        time.sleep(60)
+
+
+def run_cur_Script(parent_dir, current_working_directory, script1, script2):
+
+
+    log_file = os.path.join(parent_dir,'script2_log.txt')
+
+    # Start script1
+    process1 = run_script (parent_dir, current_working_directory, script1, log_file=log_file)
+   
+
+    # Wait for management address to appear in the log
+    mgmt_address = read_mgmt_address(log_file)
+    if mgmt_address:
+        print(f"Found management address: {mgmt_address}")
+        update_script2(script2, mgmt_address)
+    else:
+        print("Failed to find management address. Running script2 without updates.")
+
+    # Start script2
+    log_complie_file = "logging_compile_file.txt"
+    process2 = run_script_cwd(script2, log_complie_file)
+
+    # Create a monitoring process for script1
+    monitor = multiprocessing.Process(target=monitor_and_kill, args=(log_complie_file, process1))
+    monitor.start()
+
+    try:
+        # Wait for both processes to finish
+        process1.wait()
+        process2.wait()
+    except KeyboardInterrupt:
+        # Handle Ctrl+C
+        print("Interrupted. Terminating processes...")
+        os.killpg(os.getpgid(process1.pid), signal.SIGTERM)
+        os.killpg(os.getpgid(process2.pid), signal.SIGTERM)
+        monitor.terminate()
+    finally:
+        # Clean up
+        monitor.join()
+        process1.wait()
+        process2.wait()
+
+    print("All processes finished.")
+
+
+
+current_working_directory = os.getcwd()
+print(current_working_directory)
+
+# Read from JSON file
+with open(json_file, 'r') as file:
+    data = json.load(file)
+print(data)
+print(data['generated_run_scripts_local/run_1.sh'])
+
+
+
+script1 = os.path.join(parentdir,'emulator_server.sh')
+with open(script1, 'w') as file:
+    file.write(scrpt_str1)
+
+
+import subprocess
+
+# Execute each run.sh script and record results
+print(data.keys())
+
+for run_sh_path in data.keys():
+    cur_info = data[run_sh_path]
+    cur_info["error_info"] = ""
+    cur_info['Status'] = "Yet to Run"
+
+
+# Write to JSON file
+json_status_file = 'run_status.json'
+with open(json_status_file, 'w') as file:
+    json.dump( data, file, indent=4)
+
+for run_sh_path in data.keys():
+    cur_info = data[run_sh_path]
+    try:
+        # Make the script executable
+        print("Compiling on CSX with Key Changes ", data[run_sh_path]["Combination"])
+        cur_info['Status'] = "Started"
+        with open(json_status_file, 'w') as file:
+            json.dump( data, file, indent=4)
+
+        
+        subprocess.run(["chmod", "+x", run_sh_path], check=True)
+        
+        # Execute the script
+        run_cur_Script(parentdir, current_working_directory, script1, run_sh_path)
+        #result = subprocess.run([run_sh_path], capture_output=True, text=True, check=True)
+        #cur_info["error_info"] = result.stdout
+        print(run_sh_path, "  Successfully Compiled")
+        cur_info['Status'] = "Complied"
+        
+    except subprocess.CalledProcessError as e:
+        print(run_sh_path, "  Failed to Compiled")
+        cur_info['Status'] = "Failed"
+        #cur_info["error_info"] = e.stderr
+        #print(f"Error: {e.stderr}")
+
+    with open(json_status_file, 'w') as file:
+        json.dump( data, file, indent=4)
+    break
+
+# Display results
+
+
+
